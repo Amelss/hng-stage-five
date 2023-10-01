48c4ee21024cdbe5e42cb0aa78cee1bf971d7fa8
Amelss <ameleyka@gmail.com>
2023-10-01 21:51:01 +0100

    Trying to get the toggler working

--- popup/popup.js
+++ popup/popup.js
@@ -1,58 +1,89 @@
-let mediaRecorder; // Variable to store the MediaRecorder
-let recordedChunks = []; // Array to store recorded video chunks
-let desktopStream; // Store the selected desktop stream
+document.addEventListener("DOMContentLoaded", function () {
+ 
 
-// Function to start screen recording
-async function startRecording() {
-  try {
-    desktopStream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
-    });
+  let mediaRecorder;
+  let recordedChunks = [];
+  let desktopStream;
+  const cameraToggle = document.getElementById("camera-toggle");
+  const audioToggle = document.getElementById("audio-toggle");
 
-    mediaRecorder = new MediaRecorder(desktopStream);
-
-    // Event listener to handle dataavailable event and save recorded chunks
-    mediaRecorder.ondataavailable = (e) => {
-      if (e.data.size > 0) {
-        recordedChunks.push(e.data);
-      }
-    };
-
-    // Event listener to handle stop event and display the recorded video
-    mediaRecorder.onstop = () => {
-      const recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-      const blobUrl = URL.createObjectURL(recordedBlob);
-
-      // You can provide a download link here
-      const downloadLink = document.getElementById("download-link");
-      downloadLink.href = blobUrl;
-      downloadLink.style.display = "block";
-
-      recordedChunks = []; // Reset recorded chunks
-    };
-
-    mediaRecorder.start();
-    document.getElementById("start-record-button").style.display = "none";
-    document.getElementById("stop-record-button").style.display = "block";
-  } catch (error) {
-    console.error("Error starting screen recording:", error);
+ 
+  async function startCameraAccess() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+     
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+    }
   }
-}
 
-// Function to stop screen recording
-function stopRecording() {
-  if (mediaRecorder && mediaRecorder.state !== "inactive") {
-    mediaRecorder.stop();
-    desktopStream.getTracks().forEach((track) => {
-      track.stop();
-    });
-    document.getElementById("stop-record-button").style.display = "none";
+
+  const startCameraButton = document.getElementById("camera-toggle");
+  startCameraButton.addEventListener("click", startCameraAccess);
+
+
+  function updateToggleButtonPosition(toggleInput) {
+    const toggler = toggleInput.nextElementSibling.querySelector(".toggler");
+    toggler.style.left = toggleInput.checked ? "22px" : "2px";
   }
-}
 
-// Add event listeners to the buttons
-const startRecordButton = document.getElementById("start-record-button");
-const stopRecordButton = document.getElementById("stop-record-button");
 
-startRecordButton.addEventListener("click", startRecording);
-stopRecordButton.addEventListener("click", stopRecording);
+  cameraToggle.addEventListener("change", function () {
+    updateToggleButtonPosition(this);
+    console.log("camera button clicked");
+  });
+
+  audioToggle.addEventListener("change", function () {
+    updateToggleButtonPosition(this);
+    console.log("audio button clicked");
+  });
+
+  async function startRecording() {
+    try {
+      desktopStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
+
+      mediaRecorder = new MediaRecorder(desktopStream);
+
+      mediaRecorder.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          recordedChunks.push(e.data);
+        }
+      };
+
+      mediaRecorder.onstop = () => {
+        const recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+        const blobUrl = URL.createObjectURL(recordedBlob);
+
+        const downloadLink = document.getElementById("download-link");
+        downloadLink.href = blobUrl;
+        downloadLink.style.display = "block";
+
+        recordedChunks = [];
+      };
+
+      mediaRecorder.start();
+      document.getElementById("start-record-button").style.display = "none";
+      document.getElementById("stop-record-button").style.display = "block";
+    } catch (error) {
+      console.error("Error starting screen recording:", error);
+    }
+  }
+
+  function stopRecording() {
+    if (mediaRecorder && mediaRecorder.state !== "inactive") {
+      mediaRecorder.stop();
+      desktopStream.getTracks().forEach((track) => {
+        track.stop();
+      });
+      document.getElementById("stop-record-button").style.display = "none";
+    }
+  }
+
+  const startRecordButton = document.getElementById("start-record-button");
+  const stopRecordButton = document.getElementById("stop-record-button");
+
+  startRecordButton.addEventListener("click", startRecording);
+  stopRecordButton.addEventListener("click", stopRecording);
+});
